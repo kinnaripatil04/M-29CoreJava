009163a464554768fd87e90bb550fbf603c44806
Kinnari Nilesh Patil <86309276+kinnaripatil04@users.noreply.github.com>
2023-04-17 22:13:51 +0530

    day six commit

---
+++ daysix/src/org/tnsif/finalkeyword/FinalVariable.java
@@ -0,0 +1,14 @@
+//program to demonstrate on final variable
+package org.tnsif.finalkeyword;
+
+public class FinalVariable {
+	//final variable should be initialized
+	//final int num
+	
+	final int num = 20;
+	public void display() {
+		//final variable cannot be changed
+		//num = 30
+		System.out.println("Num is " + num);
+	}
+}
