7e2c34000637bf24bc0ae24e342246c41cfc7973
Kinnari Nilesh Patil <86309276+kinnaripatil04@users.noreply.github.com>
2023-06-15 23:45:09 +0530

    first commit

---
+++ daynineteen/src/org/tnsif/repository/StudentRepo.java
@@ -0,0 +1,17 @@
+package org.tnsif.repository;
+//program to demonstrate on CRUD operation
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import org.tnsif.entities.Student;
+public interface StudentRepo {
+	/*The Statement object used for executing a static 
+	 * SQL statement and returning the results it produces. 
+	 */
+	Statement makeStatement();
+	PreparedStatement createPrepareStatement(String query);
+	public int addQuery(String query,Student student);
+	public int updateQuery(String query,Student student);
+	public int deleteQuery(String query);
+	public ResultSet retrieveQuery(String query);
+}
